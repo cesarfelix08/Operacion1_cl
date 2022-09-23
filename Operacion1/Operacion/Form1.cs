fcb1b2e43b7203c6c6ff69d4d2ff28c5badb09f2
cf384 <cf384@M3>
2022-09-23 17:21:25 -0600

    Agregar archivos de proyecto.

---
+++ Operacion1/Operacion/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operacion1
+{
+        public partial class Form1
+        {
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                clresta oresta = new clresta(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+                textBox3.Text = oresta.restar().ToString();
+            }
+        }
+}
